02760bb092cc07ad056caa9e9a600341b9133301
Gs.8life <Gs.8life@gmail.com>
2024-02-01 16:49:20 +0900

    .

--- Win_API/PokemonContents/PokemonContents.cpp
+++ Win_API/PokemonContents/PokemonContents.cpp
@@ -1,1 +1,7 @@
 #include "PokemonContents.h"
+#include "PokemonContents/PokemonContents.h"
+#include "Title.h"
+#include "Play.h"
+#include <EngineBase\EngineDirectory.h>
+#include <EngineBase\EngineFile.h>
+#include <EngineCore\EngineResourcesManager.h>

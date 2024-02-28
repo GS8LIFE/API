87cdf561fbacba3dc847bd67c114c6e9981dc4ae
Gs.8life <Gs.8life@gmail.com>
2024-02-28 16:38:33 +0900

    버블 배치

--- Win_API/bobbleContents/Bobble.cpp
+++ Win_API/bobbleContents/Bobble.cpp
@@ -14,37 +14,18 @@ Bobble::~Bobble()
 void Bobble::BeginPlay()
 {
 	AActor::BeginPlay();
+	
+		UImageRenderer* Renderer = CreateImageRenderer(RenderOrder::bobble);
+		Renderer = CreateImageRenderer(RenderOrder::bobble);
+		Renderer->SetImage("Bobble.png");
+		Renderer->SetTransform({ {0,0}, {72, 72} });
+		Renderer->CreateAnimation("idle", "Bobble.png", 1, 1, 0.1f, true);
+		Renderer->CreateAnimation("R", "Bobble.png", 11, 12, 0.2f, true);
 
-	Renderer = CreateImageRenderer(RenderOrder::bobble);
-	Renderer->SetImage("Bobble.png");
-	Renderer->SetTransform({ {300,100}, {72, 72} });
-	Renderer->CreateAnimation("idle", "Bobble.png", 1, 1, 0.1f, true);
-	Renderer->CreateAnimation("wow", "Bobble.png", 1, 3, 0.2f, true);
-	std::map<int, std::vector<char>> map;
-	map[0] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'G', 'G' };
-	map[1] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'G', '/' };
-	map[2] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'R', '.' };
-	map[3] = { 'B', 'B', 'G', 'G', 'R', 'R', 'Y', '/' };
-	map[4] = { 'B', 'G', 'G', 'R', 'R', 'Y', 'Y', 'R' };
+
 
 	// 맵에 있는 백터의 원소값을 받아서 해당하는 색을 채워줌
-	for (int row_idx = 0; row_idx < map.size(); ++row_idx)
-	{
-		const auto& row = map[row_idx];
 
-		// . 혹은 / 이면 다음으로 넘김 (색이 없으므로)
-		for (int col_idx = 0; col_idx < row.size(); ++col_idx)
-		{
-			char col = row[col_idx];
-			if (col == '.' || col == '/')
-			{
-				continue;
-			}
-
-
-			//버블 추가하는 코드
-		}
-	}
 
 
 }
@@ -57,6 +38,7 @@ void Bobble::get_bubble(char _color)
 	case 'S':
 		break;
 	case 'R':
+		
 		break;
 	case 'P':
 		break;
@@ -85,11 +67,10 @@ void Bobble::Tick(float _DeltaTime)
 {
 	if (true == UEngineInput::IsDown('Q'))
 	{
-		Renderer->ChangeAnimation("wow");
 	}
 	if (true == UEngineInput::IsDown('W'))
 	{
-		Renderer->ChangeAnimation("idle");
+
 	}
 
 }
--- Win_API/bobbleContents/PlayLevel.cpp
+++ Win_API/bobbleContents/PlayLevel.cpp
@@ -9,7 +9,6 @@
 #include "Bobble.h"
 #include <EngineCore\EngineCore.h>
 #include "Enum.h"
-int a = 270;
 PlayLevel::PlayLevel() 
 {
 }
@@ -22,26 +21,26 @@ void PlayLevel::BeginPlay()
 {
 	ULevel::BeginPlay();
 
-	
-		FVector WindowScale = GEngine->MainWindow.GetWindowScale();
 
-		AActor* Back = SpawnActor<AActor>();
-		UImageRenderer* Renderer = Back->CreateImageRenderer(RenderOrder::Map);
-		Renderer->SetImage("map.png");
-		Renderer->SetPosition(WindowScale.Half2D());
-		Renderer->SetScale(WindowScale);
+	FVector WindowScale = GEngine->MainWindow.GetWindowScale();
 
-		AActor* borderline = SpawnActor<AActor>();
-		UImageRenderer* Renderer1 = borderline->CreateImageRenderer(RenderOrder::Map);
-		Renderer1->SetImage("border.png");
-		Renderer1->SetPosition({317,286});
-		Renderer1->SetScale({320,548});
+	AActor* Back = SpawnActor<AActor>();
+	UImageRenderer* Renderer = Back->CreateImageRenderer(RenderOrder::Map);
+	Renderer->SetImage("map.png");
+	Renderer->SetPosition(WindowScale.Half2D());
+	Renderer->SetScale(WindowScale);
 
-		AActor* bar = SpawnActor<AActor>();
-		UImageRenderer* Renderer3 = bar->CreateImageRenderer(RenderOrder::Map);
-		Renderer3->SetAlpha(100);
-		Renderer3->SetImage("bar.png");
-		Renderer3->SetTransform({ {325,400}, {272, 60} });
+	AActor* borderline = SpawnActor<AActor>();
+	UImageRenderer* Renderer1 = borderline->CreateImageRenderer(RenderOrder::Map);
+	Renderer1->SetImage("border.png");
+	Renderer1->SetPosition({ 317,286 });
+	Renderer1->SetScale({ 320,548 });
+
+	AActor* bar = SpawnActor<AActor>();
+	UImageRenderer* Renderer3 = bar->CreateImageRenderer(RenderOrder::Map);
+	Renderer3->SetAlpha(100);
+	Renderer3->SetImage("bar.png");
+	Renderer3->SetTransform({ {325,400}, {272, 60} });
 
 	arrow* Arrow = SpawnActor<arrow>();
 	shooter* Shooter = SpawnActor<shooter>();
@@ -50,15 +49,46 @@ void PlayLevel::BeginPlay()
 	Dragon2* Player2 = SpawnActor<Dragon2>();
 	Dragon2Tool* Dragon2T = SpawnActor<Dragon2Tool>();
 	DragonTool* Dragon1T = SpawnActor<DragonTool>();
-	Bobble* bobble1 = SpawnActor<Bobble>();
+
+	std::map<int, std::vector<char>> map;
+	map[0] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'G', 'G' };
+	map[1] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'G', '/' };
+	map[2] = { 'R', 'R', 'Y', 'Y', 'B', 'B', 'R', '.' };
+	map[3] = { 'B', 'B', 'G', 'G', 'R', 'R', 'Y', '/' };
+	map[4] = { 'B', 'G', 'G', 'R', 'R', 'Y', 'Y', 'R' };
+
+	for (int row_idx = 0; row_idx < map.size(); ++row_idx)
+	{
+		const auto& row = map[row_idx];
+
+		// . 혹은 / 이면 다음으로 넘김 (색이 없으므로)
+		for (int col_idx = 0; col_idx < row.size(); ++col_idx)
+		{
+			char col = row[col_idx];
+			if (col == '.' || col == '/')
+			{
+				continue;
+			}
+			if (row_idx % 2 == 1)
+			{
+				Bobble* NewB = SpawnActor<Bobble>();
+				NewB->SetActorLocation({ 221 + (32 * col_idx), 65 + (32 * row_idx) });
+			}
+			else
+			{
+				Bobble* NewB = SpawnActor<Bobble>();
+				NewB->SetActorLocation({ 205 + (32 * col_idx), 65 + (32 * row_idx) });
+			}
+
+			//버블 추가하는 코드
+		}
+	}
 }
 
-void PlayLevel::Tick(float _DeltaTime)
-{
+	void PlayLevel::Tick(float _DeltaTime) {
 	ULevel::Tick(_DeltaTime);
 
 	if (UEngineInput::IsDown('Q'))
 	{
-	}
-	
+	}	
 }
